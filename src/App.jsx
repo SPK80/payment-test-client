2b5c8dd742c2d411d7a2d6a5507b95babdf00bfb
SPK80 <psukonkin@gmail.com>
2022-03-20 10:03:03 +0300

    refactor: delete unnecessary components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import 'antd/dist/antd.css';
-import { Button } from "antd";
-import { CardNumber } from "./components/CardNumber";
-import { ExpirationDate } from './components/ExpirationDate';
-import { CVV } from './components/CVV';
-import { Amount } from './components/Amount';
-import { useState } from 'react';
-import './components/styles.css';
-import { postPayment } from './serverApi';
 import { PaymentForm } from './components/PaymentForm';
 
 function App() {
--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,3 +1,4 @@
+import 'antd/dist/antd.css';
 import React, { useState } from 'react';
 import { Form, Button, DatePicker, Input } from 'antd';
 import { postPayment } from '../serverApi';
